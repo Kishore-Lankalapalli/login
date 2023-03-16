ba2c6ac66d5ce404aa8b705c0498d7a5c37d155a
Kishore-Lankalapalli <klankalapalli369@gmail.com>
2023-03-16 09:40:22 +0000

    login

---
+++ index.js
@@ -0,0 +1,64 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const dbPath = path.join(__dirname, "user.db");
+const { open } = require("sqlite");
+const sqlite3 = require("sqlite3");
+const jwt = require("jsonwebtoken");
+const cors = require("cors");
+
+app.use(express.json());
+
+app.use(cors());
+
+let db = null;
+const initialiseDbAndServer = async () => {
+  try {
+    db = await open({
+      filename: dbPath,
+      driver: sqlite3.Database,
+    });
+    app.listen(3000, () => {
+      console.log("Server Running Successfully at localhost://3001");
+    });
+  } catch (e) {
+    console.log(`${e.message}`);
+    process.exit(1);
+  }
+};
+initialiseDbAndServer();
+
+app.get("/", async (request, response) => {
+  const getUserQuery = `SELECT * FROM user WHERE name = "kishore";`;
+  const userDetails = await db.get(getUserQuery);
+  response.send(userDetails);
+});
+
+app.post("/login", async (request, response) => {
+  console.log(request.body);
+  const { email, password } = request.body;
+
+  const userQuery = `SELECT * FROM user WHERE email='${email}';`;
+
+  const dbUser = await db.run(userQuery);
+
+  if (dbUser === undefined) {
+    response.status(400);
+    response.send("Invalid User");
+  } else {
+    if (password === dbUser.password) {
+      if (password.length < 8) {
+        response.send("Password length must be 8 characters");
+      } else {
+        const payLoad = {
+          email: email,
+        };
+        const jwtToken = jwt.sign(payLoad, "secret_token");
+        response.status(200);
+        response.send({ jwtToken });
+      }
+    }
+  }
+});
+
+module.exports = app;
